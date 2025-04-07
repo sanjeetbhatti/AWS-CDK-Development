5c9a3394812e4375678b7fdb79dca2d6e728cec0
sanjeetbhatti <edu.sanjeet@gmail.com>
2025-04-07 13:46:10 -0600

    Set up REST API and Lambda function
    AWS CDK stack links an API Gateway to the Lambda function, creating a REST API. HTTP requests to the API Gateway trigger the Lambda handler to process the requests and return responses (JSON).

--- py_serverless_rest_api/py_serverless_rest_api_stack.py
+++ py_serverless_rest_api/py_serverless_rest_api_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigateway,
+    aws_lambda
 )
 from constructs import Construct
 
@@ -10,7 +10,23 @@ class PyServerlessRestApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Creating a lambda
+        employee_lambda = aws_lambda.Function(
+             self,
+             "EmployeeLambda",
+             runtime=aws_lambda.Runtime.PYTHON_3_11,
+             code=aws_lambda.Code.from_asset("services"), 
+             handler="index.handler", # filename.function
+         )
+        
+        # Building api and attaching it to the stack
+        api = aws_apigateway.RestApi(self, "Employee-Api")
+        employee_resource = api.root.add_resource("employee")
+        
+        # Attaching the lambda to the api gateway
+        employee_lambda_integration = aws_apigateway.LambdaIntegration(employee_lambda) #lambda integration object from api gateway
+        employee_resource.add_method("GET", employee_lambda_integration) # two methods 'get and post' added
+        employee_resource.add_method("POST", employee_lambda_integration)
 
         # example resource
         # queue = sqs.Queue(
