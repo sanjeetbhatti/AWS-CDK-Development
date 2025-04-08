9f42deeacebfb4d49aa7cc19847d689d59933986
sanjeetbhatti <edu.sanjeet@gmail.com>
2025-04-08 15:04:20 -0600

    Create DynamoDB table and grant R/W access to Lambda

--- py_serverless_rest_api/py_serverless_rest_api_stack.py
+++ py_serverless_rest_api/py_serverless_rest_api_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     Stack,
     aws_apigateway,
-    aws_lambda
+    aws_lambda,
+    aws_dynamodb
 )
 from constructs import Construct
 
@@ -10,6 +11,16 @@ class PyServerlessRestApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Creating DynamoDB table that will be accessible inside lambda
+        employee_table = aws_dynamodb.TableV2(
+            self,
+            "EmployeeTable",
+            partition_key=aws_dynamodb.Attribute(
+                name="id", type=aws_dynamodb.AttributeType.STRING
+            ),
+            billing=aws_dynamodb.Billing.on_demand()
+        )
+
         # Creating a lambda
         employee_lambda = aws_lambda.Function(
              self,
@@ -17,8 +28,12 @@ class PyServerlessRestApiStack(Stack):
              runtime=aws_lambda.Runtime.PYTHON_3_11,
              code=aws_lambda.Code.from_asset("services"), 
              handler="index.handler", # filename.function
+             environment={"TABLE_NAME": employee_table.table_name} # adding reference to the table
          )
         
+        # Granting access to lambda to perform read & write operations inside the table 
+        employee_table.grant_read_write_data(employee_lambda)
+
         # Building api and attaching it to the stack
         api = aws_apigateway.RestApi(self, "Employee-Api")
         employee_resource = api.root.add_resource("employee")
@@ -27,9 +42,3 @@ class PyServerlessRestApiStack(Stack):
         employee_lambda_integration = aws_apigateway.LambdaIntegration(employee_lambda) #lambda integration object from api gateway
         employee_resource.add_method("GET", employee_lambda_integration) # two methods 'get and post' added
         employee_resource.add_method("POST", employee_lambda_integration)
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PyServerlessRestApiQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
--- services/index.py
+++ services/index.py
@@ -1,7 +1,36 @@
 import json
+import boto3
+import os
+import uuid # creates random IDs
  
+# Initializing table client outside the handler function for reusability.
+table_name = os.environ.get("TABLE_NAME")
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(table_name)
+
 def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps("Hello World!"),
-    }
+    method = event["httpMethod"]
+
+    if method == "POST":
+        item = json.loads(event["body"])
+        item["id"] = str(uuid.uuid4())
+        table.put_item(Item=item)
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"id": item["id"]}),
+        }
+
+    if method == "GET":
+        employee_id = event["queryStringParameters"]["id"]
+        response = table.get_item(Key={"id": employee_id})
+
+        if "Item" in response:
+            return {
+                "statusCode": 200,
+                "body": json.dumps(response["Item"]),
+            }
+        else:
+            return {
+                "statusCode": 404,
+                "body": json.dumps("Not found"),
+            }
